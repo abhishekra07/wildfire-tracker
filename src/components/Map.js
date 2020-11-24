59e3901326b3de34769427f1de2570b6594ee53c
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-24 23:43:21 +0530

    completed wildfire tracker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfo from './LocationInfo'
 
 const Map = ({eventData, center, zoom}) => {
+  const [locationInfo, setLocationinfo] = useState(null)
+
   const markers = eventData.map( event => {
     if(event.categories[0].id == 8){
-      return <LocationMarker lat= { event.geometries[0].coordinates[1] } lng= { event.geometries[0].coordinates[0] } />
+      return <LocationMarker lat= { event.geometries[0].coordinates[1] } lng= { event.geometries[0].coordinates[0] } onClick={ () => {
+        setLocationinfo({id: event.id, title: event.title})
+      }}/>
     }
     return null
   })
@@ -12,12 +18,13 @@ const Map = ({eventData, center, zoom}) => {
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{key: YOUR_API_KEY}}
+        bootstrapURLKeys={{key: PUT_YOUR_API_KEY_HERE}}
         defaultCenter={ center }
         defaultZoom= { zoom }
       >
         { markers }
       </GoogleMapReact>
+      { locationInfo && <LocationInfo info={locationInfo} />}
     </div>
   )
 }
